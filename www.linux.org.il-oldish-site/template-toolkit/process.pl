28f85694a3fbebe24309d245d83d5c2844d21ff6
Shlomi Fish <shlomif@shlomifish.org>
2020-06-30 20:13:53 +0300

    cleanups and updates [old site]

--- www.linux.org.il-oldish-site/template-toolkit/process.pl
+++ www.linux.org.il-oldish-site/template-toolkit/process.pl
@@ -5,71 +5,71 @@ use warnings;
 
 use utf8;
 
-use Template;
-use File::Find::Object;
+use Template           ();
+use File::Find::Object ();
 
 use File::Path qw( mkpath );
-use File::Spec;
+use File::Spec ();
 use File::Copy qw( copy );
 
 my $template = Template->new(
     {
         INCLUDE_PATH => ".",
-        POST_CHOMP => 1,
-        RELATIVE => 1,
-        ENCODING => 'utf8',
+        POST_CHOMP   => 1,
+        RELATIVE     => 1,
+        ENCODING     => 'utf8',
     }
 );
 
+my $tree = File::Find::Object->new( {}, './src' );
 
-my $tree = File::Find::Object->new({}, './src');
-
-while (my $result = $tree->next_obj())
+while ( my $result = $tree->next_obj() )
 {
-    if ($result->is_dir())
+    if ( $result->is_dir() )
     {
-        if ($result->path() eq './dest')
-        {
-            $tree->prune();
-        }
-        else
-        {
-            mkpath(File::Spec->catdir(
-                    File::Spec->curdir(), "dest", @{$result->full_components()}
-                )
-            );
-        }
+        mkpath(
+            File::Spec->catdir(
+                File::Spec->curdir(), "dest",
+                @{ $result->full_components() }
+            )
+        );
     }
     else
     {
         my $basename = $result->basename;
-        if ($basename =~ s/\.html\.tt2\z/.html/)
+        if ( $basename =~ s/\.html\.tt2\z/.html/ )
         {
-            my $base_path = ('../' x scalar( @{$result->dir_components()} ));
+            my $base_path =
+                ( '../' x scalar( @{ $result->dir_components() } ) );
 
-            my $vars =
-            +{
+            my $vars = +{
                 base_path => $base_path,
-                icon_en => qq#<img src="${base_path}icon_lang_en.png" alt="סמל אנגלית" class="symbol" /> #,
-                icon_he => qq#<img src="${base_path}icon_lang_he.png" alt="סמל עברית" class="symbol" /> #,
+                icon_en =>
+qq#<img src="${base_path}icon_lang_en.png" alt="סמל אנגלית" class="symbol" /> #,
+                icon_he =>
+qq#<img src="${base_path}icon_lang_he.png" alt="סמל עברית" class="symbol" /> #,
             };
 
-            $template->process($result->path(), $vars,
-                File::Spec->catfile(File::Spec->curdir(), "dest",
-                    @{$result->dir_components()}, $basename),
+            $template->process(
+                $result->path(),
+                $vars,
+                File::Spec->catfile(
+                    File::Spec->curdir(),           "dest",
+                    @{ $result->dir_components() }, $basename
+                ),
                 binmode => ':utf8',
-            )
-                or die $template->error();
+            ) or die $template->error();
         }
-        elsif (
-            $basename !~ /~\z/
-            && ( !($basename =~ /\A\./ && $basename =~ /\.swp\z/) )
-            && ($basename ne 'process.pl')
-        )
+        elsif ($basename !~ /~\z/
+            && ( !( $basename =~ /\A\./ && $basename =~ /\.swp\z/ ) )
+            && ( $basename ne 'process.pl' ) )
         {
-            copy($result->path,
-                File::Spec->catfile(File::Spec->curdir(), "dest",
-                    @{$result->dir_components()}, $basename),
+            copy(
+                $result->path,
+                File::Spec->catfile(
+                    File::Spec->curdir(),           "dest",
+                    @{ $result->dir_components() }, $basename
+                ),
             );
         }
     }
